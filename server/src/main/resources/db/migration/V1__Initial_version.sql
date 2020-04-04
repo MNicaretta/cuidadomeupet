d56c869940b533bb7801e5a9dfa9b33320b07d0b
Marcelo Nicaretta <marcelo.nicaretta@universo.univates.br>
2020-04-04 14:35:02 -0300

    Ajustadas heranças das tabelas

--- server/src/main/resources/db/migration/V1__Initial_version.sql
+++ server/src/main/resources/db/migration/V1__Initial_version.sql
@@ -15,14 +15,17 @@ CREATE TABLE characteristics (
 ALTER TABLE characteristics ADD CONSTRAINT characteristics_entity_fkey FOREIGN KEY (entity_id, entity_revision) REFERENCES entities;
 
 CREATE TABLE users (
+    id INTEGER NOT NULL,
+    revision INTEGER NOT NULL,
     name TEXT NOT NULL,
     email TEXT NOT NULL,
     password TEXT NOT NULL,
     identity TEXT NOT NULL,
     phone TEXT NOT NULL
-) inherits (entities);
+);
 
 ALTER TABLE users ADD CONSTRAINT users_pkey PRIMARY KEY (id, revision);
+ALTER TABLE users ADD CONSTRAINT users_fkey FOREIGN KEY (id, revision) REFERENCES entities;
 
 CREATE TABLE banking_datas (
     id SERIAL PRIMARY KEY,
@@ -36,25 +39,33 @@ CREATE TABLE banking_datas (
 ALTER TABLE banking_datas ADD CONSTRAINT banking_datas_user_fkey FOREIGN KEY (user_id, user_revision) REFERENCES users;
 
 CREATE TABLE pets (
+    id INTEGER NOT NULL,
+    revision INTEGER NOT NULL,
     name TEXT NOT NULL,
     additional_info TEXT NOT NULL,
     user_id INTEGER NOT NULL,
-    user_revision INTEGER NOT NULL,
-) inherits (entities);
+    user_revision INTEGER NOT NULL
+);
 
 ALTER TABLE pets ADD CONSTRAINT pets_pkey PRIMARY KEY (id, revision);
+ALTER TABLE pets ADD CONSTRAINT pets_fkey FOREIGN KEY (id, revision) REFERENCES entities;
 ALTER TABLE pets ADD CONSTRAINT pets_user_fkey FOREIGN KEY (user_id, user_revision) REFERENCES users;
 
 CREATE TABLE addresses (
+    id INTEGER NOT NULL,
+    revision INTEGER NOT NULL,
     type SMALLINT NOT NULL,
     size SMALLINT NOT NULL,
     address TEXT NOT NULL,
     zip TEXT NOT NULL
-) inherits (entities);
+);
 
 ALTER TABLE addresses ADD CONSTRAINT addresses_pkey PRIMARY KEY (id, revision);
+ALTER TABLE addresses ADD CONSTRAINT addresses_fkey FOREIGN KEY (id, revision) REFERENCES entities;
 
 CREATE TABLE services (
+    id INTEGER NOT NULL,
+    revision INTEGER NOT NULL,
     type SMALLINT NOT NULL,
     price REAL NOT NULL,
     distance NUMERIC,
@@ -63,14 +74,15 @@ CREATE TABLE services (
     user_revision INTEGER NOT NULL,
     address_id INTEGER,
     address_revision INTEGER
-) inherits (entities);
+);
 
 ALTER TABLE services ADD CONSTRAINT services_pkey PRIMARY KEY (id, revision);
+ALTER TABLE services ADD CONSTRAINT services_fkey FOREIGN KEY (id, revision) REFERENCES entities;
 ALTER TABLE services ADD CONSTRAINT services_user_fkey FOREIGN KEY (user_id, user_revision) REFERENCES users;
 ALTER TABLE services ADD CONSTRAINT services_address_fkey FOREIGN KEY (address_id, address_revision) REFERENCES addresses;
 
 CREATE TABLE orders (
-    id SERIAL PRIMARY KEY
+    id SERIAL PRIMARY KEY,
     start_date TIMESTAMP NOT NULL,
     end_date TIMESTAMP NOT NULL,
     total_value REAL NOT NULL,
@@ -93,7 +105,7 @@ CREATE TABLE evaluations (
     commentaries TEXT NOT NULL,
     order_id INTEGER NOT NULL REFERENCES orders,
     user_id INTEGER NOT NULL,
-    user_revision INTEGER NOT NULL,
+    user_revision INTEGER NOT NULL
 );
 
 ALTER TABLE evaluations ADD CONSTRAINT evaluations_user_fkey FOREIGN KEY (user_id, user_revision) REFERENCES users;
