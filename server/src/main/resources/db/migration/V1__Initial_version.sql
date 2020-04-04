8a1e8f37c99cc407adefc62e88b83f9ba0fd304e
Marcelo Nicaretta <marcelo.nicaretta@universo.univates.br>
2020-04-04 13:48:03 -0300

    Adicionadas tabelas restantes

--- server/src/main/resources/db/migration/V1__Initial_version.sql
+++ server/src/main/resources/db/migration/V1__Initial_version.sql
@@ -43,4 +43,66 @@ CREATE TABLE pets (
 ) inherits (entities);
 
 ALTER TABLE pets ADD CONSTRAINT pets_pkey PRIMARY KEY (id, revision);
-ALTER TABLE pets ADD CONSTRAINT pets_user_fkey FOREIGN KEY (user_id, user_revision) REFERENCES user;
+ALTER TABLE pets ADD CONSTRAINT pets_user_fkey FOREIGN KEY (user_id, user_revision) REFERENCES users;
+
+CREATE TABLE addresses (
+    type SMALLINT NOT NULL,
+    size SMALLINT NOT NULL,
+    address TEXT NOT NULL,
+    zip TEXT NOT NULL
+) inherits (entities);
+
+ALTER TABLE addresses ADD CONSTRAINT addresses_pkey PRIMARY KEY (id, revision);
+
+CREATE TABLE services (
+    type SMALLINT NOT NULL,
+    price REAL NOT NULL,
+    distance NUMERIC,
+    state SMALLINT NOT NULL,
+    user_id INTEGER NOT NULL,
+    user_revision INTEGER NOT NULL,
+    address_id INTEGER,
+    address_revision INTEGER
+) inherits (entities);
+
+ALTER TABLE services ADD CONSTRAINT services_pkey PRIMARY KEY (id, revision);
+ALTER TABLE services ADD CONSTRAINT services_user_fkey FOREIGN KEY (user_id, user_revision) REFERENCES users;
+ALTER TABLE services ADD CONSTRAINT services_address_fkey FOREIGN KEY (address_id, address_revision) REFERENCES addresses;
+
+CREATE TABLE orders (
+    id SERIAL PRIMARY KEY
+    start_date TIMESTAMP NOT NULL,
+    end_date TIMESTAMP NOT NULL,
+    total_value REAL NOT NULL,
+    state SMALLINT NOT NULL,
+    service_id INTEGER NOT NULL,
+    service_revision INTEGER NOT NULL,
+    user_id INTEGER NOT NULL,
+    user_revision INTEGER NOT NULL,
+    address_id INTEGER,
+    address_revision INTEGER
+);
+
+ALTER TABLE orders ADD CONSTRAINT orders_service_fkey FOREIGN KEY (service_id, service_revision) REFERENCES services;
+ALTER TABLE orders ADD CONSTRAINT orders_user_fkey FOREIGN KEY (user_id, user_revision) REFERENCES users;
+ALTER TABLE orders ADD CONSTRAINT orders_address_fkey FOREIGN KEY (address_id, address_revision) REFERENCES addresses;
+
+CREATE TABLE evaluations (
+    id SERIAL PRIMARY KEY,
+    grade SMALLINT not null,
+    commentaries TEXT NOT NULL,
+    order_id INTEGER NOT NULL REFERENCES orders,
+    user_id INTEGER NOT NULL,
+    user_revision INTEGER NOT NULL,
+);
+
+ALTER TABLE evaluations ADD CONSTRAINT evaluations_user_fkey FOREIGN KEY (user_id, user_revision) REFERENCES users;
+
+CREATE TABLE orders_has_pets (
+    order_id INTEGER NOT NULL REFERENCES orders,
+    pet_id INTEGER NOT NULL,
+    pet_revision INTEGER NOT NULL
+);
+
+ALTER TABLE orders_has_pets ADD CONSTRAINT orders_has_pets_pkey PRIMARY KEY (order_id, pet_id, pet_revision);
+ALTER TABLE orders_has_pets ADD CONSTRAINT orders_has_pets_pet_fkey FOREIGN KEY (pet_id, pet_revision) REFERENCES pets;
