479dc8b36ff63c63640d8d48842fbb288d6bbaff
Arthur <arthur.m.saraiva@hotmail.com>
2020-05-06 00:36:02 -0300

    #14 Ajustes no header
    Realizar ajustes no header para redirecionar o signin, siginup e profile corretamente.

--- app/src/app/core/header/header.component.ts
+++ app/src/app/core/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../auth/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +9,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  userName: string;
+
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
+    this.authService.currentUser.subscribe(value => this.userName = value);
   }
 
+  logout() {
+    this.authService.signout();
+    this.router.navigate(['signin']);
+  }
 }
